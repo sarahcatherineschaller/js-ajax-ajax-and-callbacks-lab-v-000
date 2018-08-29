6166b53673ddecd81b5c0d6006fa021ddfa5e977
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-29 15:03:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.");
 
+var searchRepositories = () => {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    $('#results').html(renderSearchResults(data))
+  }).fail(error => {
+    displayError()
+  });
+};
 
 $(document).ready(function (){
 });
